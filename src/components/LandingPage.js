e40bea24857cca368f0845ffc15190f07023d030
Riki Konikoff <riki.konikoff@gmail.com>
2017-07-14 13:14:16 -0400

    get assets from designers

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import '../stylesheets/LandingPage.css'
-import icnGoogleplus from '../assets/icnGoogleplus.png'
+import '../stylesheets/LandingPage.css';
+import icGooglePlus from '../stylesheets/CaravanWeb_Assets/ic-google-plus.png';
 
 export class LandingPage extends Component {
   render(){
     return(
       <div className='container'>
         <div className='Mask'>
-          <p></p>
+          <div className='logo'>
+            Caravan
+          </div>
         </div>
         <div className='white-background'>
           <div className='Tagline'>
@@ -19,7 +21,7 @@ export class LandingPage extends Component {
           </div>
           <button className='google-button'>
             <div className='gplus'>
-              <img src={icnGoogleplus} alt=''/>
+              <img src={icGooglePlus} alt=''/>
             </div>
             <div className='Log-in-with-Google'>Log in with Google</div>
           </button>
